82f7e4c637cf495529235f95d4100bbc7d928bad
Senai Informática <thiel_13@hotmail.com>
2018-01-17 15:23:37 -0300

    change info style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import './header.less';
 
 const Header = () => {
   return (
-    <header className="container header row -justify-content-center">
+    <header className="container header row -justify-content-center _margin">
       <Link to="/app">
         <img src={LogoTipo} alt="Logo Cálculo de Tarifas" className="logo"/>
       </Link>
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 import ListEquipments from "../../components/ListEquipments";
 
 const MainPage = () => {
   return (
-    <div>
-      <ListEquipments/>
-    </div>
+    <Row>
+      <Col span={12} offset={6}>
+        <ListEquipments/>
+      </Col>
+    </Row>
   )
 };
 
