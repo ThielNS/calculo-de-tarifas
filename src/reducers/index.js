3f3d681a01c3ce1fea30d5b5d79d879d65df8db9
Juliana Cerqueira de Abreu <juliana.cerqueira55@gmail.com>
2018-02-01 10:22:21 -0200

    changes in monthly

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,15 @@
-import { combineReducers } from 'redux';
-import equipmentsReducer from './equipmentsReducer';
-import modalReducer from './modalReducer';
-import { powerDistribuitorReducer } from './powerDistribuitorReducer';
+import { combineReducers } from "redux";
+import equipmentsReducer from "./equipmentsReducer";
+import modalReducer from "./modalReducer";
+import { powerDistribuitorReducer } from "./powerDistribuitorReducer";
 import listMonthly from "./chooseMonthlyReducer";
 
-const reducerApp = combineReducers({ equipmentsReducer, modalReducer, powerDistribuitorReducer, listMonthly });
+const reducerApp = combineReducers({
+  equipmentsReducer,
+  modalReducer,
+  powerDistribuitorReducer,
+  listMonthly
+});
 
 export default reducerApp;
+
